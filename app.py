51934434f0398d74b0d86557cbf625da3a03dc48
jmeer <j.vandermeer@student.science.ru.nl>
2021-11-16 16:35:53 +0100

    initial commit

---
+++ app.py
@@ -0,0 +1,29 @@
+from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/results.db'
+db = SQLAlchemy(app)
+
+db.create_all()
+
+class Result(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    match = db.Column(db.String(120))
+    year = db.Column(db.String(120))
+    field = db.Column(db.String(120))
+    totalTime = db.Column(db.String(120))
+
+    def __repr__(self):
+        return f'id: {self.id}, match: {self.match}, year: {self.year}, field: {self.field}, total time: {self.totalTime}'
+
+#@app.before_first_request
+#def create_tables():
+#    db.create_all()
+
+@app.route("/")
+def main():
+  return render_template("index.html")
+
+if __name__ == "__main__":
+  app.run()
